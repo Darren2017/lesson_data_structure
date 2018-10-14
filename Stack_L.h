f065e18e0c32fac20480371fe6afbe2488ec293d
Darren2017 <17362990052@163.com>
2018-10-14 21:13:04 +0800

    feat:Stack

---
+++ Stack_L.h
@@ -0,0 +1,86 @@
+#include<iostream>
+#include <iomanip>
+
+#define ElemType int
+
+using namespace std;
+
+typedef struct LNode{
+    ElemType data;
+    struct LNode *next;
+}LNode, *LinkStack;
+
+void InitStack_L(LinkStack &S){
+    S = NULL;
+}
+
+void DestroyStack(LinkStack &S){
+    LinkStack q;
+    while(S){
+        q = S;
+        S = S -> next;
+        delete q;
+    }
+}
+
+void ClearStack(LinkStack &S){
+    LinkStack q;
+    while(S){
+        q = S;
+        S = S -> next;
+        delete q;
+    }
+}
+
+bool StackEmpty(LinkStack S){
+    if(!S){
+        return true;
+    }
+    return false;
+}
+
+int StackLength(LinkStack S){
+    int i = 0;
+    LinkStack q;
+    while(S){
+        i++;
+        q = S;
+        S = S -> next;
+    }
+    return i;
+}
+
+bool GetTop(LinkStack S, ElemType &e){
+    if(S){
+        e = S -> data;
+        return true;
+    }
+    return false;
+}
+
+void Push(LinkStack &S, ElemType &e){
+    LinkStack q = new LNode;
+    q -> data = e;
+    q -> next = S;
+    S = q;
+}
+
+bool Pop(LinkStack &S, ElemType &e){
+    if(!S){
+        return false;
+    }
+    e = S -> data;
+    LinkStack q = S;
+    S = S -> next;
+    delete q;
+    return true;
+}
+
+void StackTraverse(LinkStack S){
+    LinkStack q = S;
+    while(q){
+        cout.setf(ios::left);
+        cout << setw(5) << q -> data;
+        q = q -> next;
+    }
+}
---
+++ Stack_Sq.h
@@ -0,0 +1,80 @@
+#include<iostream>
+#include <iomanip>
+
+#define SElemType int
+#define SIZE 100
+#define INCREMENTSIZE 20
+
+using namespace std;
+
+typedef struct{
+    SElemType *elem;
+    int top;
+    int stacksize;
+    int incrementsize;
+} SqStack;
+
+void InitStack_Sq(SqStack &S){
+    S.elem = new SElemType[SIZE];
+    S.top = -1;
+    S.stacksize = SIZE;
+    S.incrementsize = INCREMENTSIZE;
+}
+
+void DestroyStack_Sq(SqStack &S){
+    delete S.elem;
+    S.top = -1;
+    S.stacksize = 0;
+    S.incrementsize = 0;
+}
+
+void ClearStack_Sq(SqStack &S){
+    S.top = -1;
+}
+
+bool StackEmpty_Sq(SqStack S){
+    if(S.top == -1){
+        return true;
+    }
+    return false;
+}
+
+int StackLength_Sq(SqStack S){
+    return S.top + 1;
+}
+
+bool GetTop_Sq(SqStack S, SElemType &e){
+    if(S.top != -1){
+        e = S.elem[S.top];
+        return true;
+    }
+    return false;
+}
+
+void Push_Sq(SqStack &S, SElemType &e){
+    if(S.top == S.stacksize - 1){
+        S.stacksize += S.incrementsize;
+        SElemType *tem = new SElemType[S.stacksize];
+        for(int i = 0; i <= S.top; i++){
+            tem[i] = S.elem[i];
+        }
+        tem[++S.top] = e;
+        delete [] S.elem;
+        S.elem = tem;
+    }
+}
+
+bool Pop_Sq(SqStack &S, SElemType &e){
+    if(S.top == -1){
+        return false;
+    }
+    e = S.elem[S.top--];
+    return true;
+}
+
+void StackTraverse_Sq(SqStack S){
+    for(int i = S.top; i >= 0; --i){
+        cout.setf(ios::left);
+        cout << setw(5) << S.elem[i];
+    }
+}
