c1a9319e6572973e875a52934cd798a917109883
Darren2017 <17362990052@163.com>
2018-11-15 22:27:46 +0800

    基数排序

--- sort/student_sort.cpp
+++ sort/student_sort.cpp
@@ -7,14 +7,14 @@
 using namespace std;
 
 typedef struct{
-    string name, id;
-    int score;
+    string name, id, score;
     int keys[10];
     int numkey;
 }RcdType;
 
 void RadixSort(RcdType s[], int cnt);
-void RadixPass(RcdType A[], RcdType B[], int cnt, int i);
+void RadixPass(RcdType A[], RcdType B[], int cnt, int i);   //降序
+void RadixPass2(RcdType A[], RcdType B[], int cnt, int i);  //升序
 
 int main()
 {
@@ -24,17 +24,16 @@ int main()
     RcdType student[n];
     for(int i = 0; i < n; i++){
         cout << "请输入学生" << i + 1 << "的学号   名字   成绩" << endl;
-        cin >> student[i].id >> student[i].name >> score;
-        student[i].score = score;
-        for(int j = 0; score; j++){
-            student[i].keys[j] = score % 10;
-            score /= 10;
+        cin >> student[i].id >> student[i].name >> student[i].score;
+        int len = student[i].score.size();
+        for(int j = 0; j < len; j++){
+            student[i].keys[j] = student[i].score[j];
         }
-        student[i].numkey = 2;
+        student[i].numkey = len;
     }
     RadixSort(student, n);
-    cout << "学号 姓名 成绩" << endl;
-    for(int i = n - 1; i >= 0; i--){
+    cout << "学号         姓名   成绩" << endl;
+    for(int i = 0; i < n; i++){
         cout << student[i].id << "   " << student[i].name << "   " << student[i].score <<  endl;
     }
     return 0;
@@ -44,10 +43,10 @@ void RadixSort(RcdType s[], int cnt){
     RcdType temstu[cnt];
     int i = s[0].numkey - 1;
     while(i >= 0){
-        RadixPass(s, temstu, cnt, i);
+        RadixPass2(s, temstu, cnt, i);
         i--;
         if(i >= 0){
-            RadixPass(temstu, s, cnt, i);
+            RadixPass2(temstu, s, cnt, i);
             i--;
         }else{
             for(int j = 0; j < cnt; j++){
@@ -63,14 +62,39 @@ void RadixPass(RcdType A[], RcdType B[], int cnt, int i){
         count[j] = 0;
     }
     for(int k = 0; k < cnt; k++){
-        count[A[k].keys[i]]++;
+        count[A[k].keys[i] - '0']++;
     }
     for(int j = 1; j < RADIX; j++){
-        count[j] = count[j - 1] +count[j];
+        count[j] = count[j - 1] + count[j];
     }
     for(int k = cnt - 1; k >= 0; k--){
-        int j = A[k].keys[i];
+        int j = A[k].keys[i] - '0';
         B[count[j] - 1] = A[k];
         count[j]--;
     }
 }
+
+void RadixPass2(RcdType A[], RcdType B[], int cnt, int i){
+    int count[11];
+    for(int j = 0; j < RADIX; j++){
+        count[j] = 0;
+    }
+    for(int k = 0; k < cnt; k++){
+        count[A[k].keys[i] - '0']++;
+    }
+    for(int j = RADIX - 2; j >= 0; j--){
+        count[j] = count[j + 1] +count[j];
+    }
+    for(int k = 0; k < cnt; k++){
+        int j = A[k].keys[i] - '0';
+        B[count[j] - 1] = A[k];
+        count[j]--;
+    }
+}
+
+/* 测试用例
+3
+2017211000 张三 072
+2017211001 李四 482
+2017211002 王五 993
+*/
