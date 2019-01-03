44ac9190178162cf1a2dfb667256e8886088c891
Darren2017 <17362990052@163.com>
2019-01-03 15:03:36 +0800

    .

--- map/Dijkstra.cpp
+++ map/Dijkstra.cpp
@@ -1,158 +1,99 @@
-// 无向有权图
 #include<iostream>
-#include<iomanip>
-#include<cstring>
+#include<algorithm>
 
 using namespace std;
 
-#define MAXVEX 100
-#define INFINITY 65535
+typedef struct edge{
+    int x, y, w;
+}edge;          //存放每条边
+int *p;     //并查集
+char *vex;      //元素信息
 
-typedef int EdgeType;
-typedef char VertexType;
-
-struct Dist{
-    string path;
-    int value;
-    bool visited;
-    Dist(){
-        visited = false;
-        value = 0;
-        path = "";
-    }
-};
-
-typedef struct
-{
-    VertexType *vexs;
-    EdgeType **arcs;
-    int numNodes, numEdges;
-    Dist *dist;
-} MGraph;
-
-void CreateMGraph(MGraph& MG);
-int Locate(MGraph MG, VertexType v);
-void dijkstra(MGraph& MG);
+static bool cmp(edge a, edge b);
+int root(int a);
+int connected(int a, int b);
+int Locate(int numNodes, char v);
+void Union(int a, int b);
+void kruskal(edge *e, int numNodes, int numEdges);
 
 int main()
 {
-    MGraph MG;
-    CreateMGraph(MG);
-    dijkstra(MG);
+    int numNodes, numEdges;
+    cout << "输入边的个数，点的个数" << endl;
+    cin >> numEdges >> numNodes;
+    edge *e = new edge[numEdges];
+    p = new int[numNodes + 1];
+    vex = new char[numNodes + 1];
+    cout << "请输入顶点信息：" << endl;
+    for(int i = 1; i <= numNodes; i++){
+        cin >> vex[i];
+    }
+    cout << "分别输入所有的边的定点、权重" << endl;
+    for(int i = 0; i < numEdges; i++){          //构造网
+        char a, b;
+        cin >> a >> b >> e[i].w;
+        e[i].x = Locate(numNodes, a);
+        e[i].y = Locate(numNodes, b);
+    }
+    for(int i = 1; i <= numNodes; i++){         // 初始化并查集，使每个节点的根节点是自身
+        p[i] = i;
+    }
+    kruskal(e, numNodes, numEdges);             //调用Kruskal算法
 
     return 0;
 }
 
-int Locate(MGraph MG, VertexType v){
-    for(int i = 1; i <= MG.numNodes; i++){
-        if(MG.vexs[i] == v){
+void kruskal(edge *e, int numNodes, int numEdges){
+    sort(e, e + numEdges, cmp);                 //对所有边进行升序排列
+    for(int i = 0; i < numEdges; i++){ 
+        if(!connected(e[i].x, e[i].y)){         //如果没有连接，则进行连接
+            Union(e[i].x, e[i].y);
+            cout << vex[e[i].x] << "-->" << vex[e[i].y] << "  " << e[i].w << endl;
+        }
+    }
+}
+
+int Locate(int numNodes, char v){
+    for(int i = 1; i <= numNodes; i++){
+        if(vex[i] == v){
             return i;
         }
     }
     return 0;
 }
 
-void CreateMGraph(MGraph& MG){
-    cout << "请输入顶点数和边数：" << endl;
-    cin >> MG.numNodes >> MG.numEdges;      //输入点数和边数
-    MG.vexs = new VertexType[MG.numNodes];  //动态申请内存
-    MG.arcs = new EdgeType *[MG.numNodes];
-    for(int i = 0; i < MG.numNodes; i++){
-        MG.arcs[i] = new EdgeType[MG.numNodes];
-    }
-    MG.dist = new Dist[MG.numNodes];
 
-    cout << "请输入顶点信息：" << endl;
-    for(int i = 0; i < MG.numNodes; i++){
-        cin >> MG.vexs[i];
-    }
-    for(int i = 0; i < MG.numNodes; i++){       //初始化邻接矩阵
-        for(int j = 0; j < MG.numNodes; j++){
-            if(i == j){
-                MG.arcs[i][j] = 0;
-            }else{
-                MG.arcs[i][j] = INFINITY;
-            }
-        }
-    }
-    for(int k = 0; k < MG.numEdges; k++){       //构造邻接矩阵
-        char v1, v2;
-        int w;
-        cout << "请输入边(vi, vj)和权值w：" << endl;
-        cin >> v1 >> v2 >> w;
-        int i = Locate(MG, v1), j = Locate(MG, v2);
-        MG.arcs[i][j] = w;
-    }
+static bool cmp(edge a, edge b){
+    return a.w < b.w;
+}
 
-    for(int i = 0; i < MG.numNodes; i++){       //输出邻接矩阵
-        for(int j = 0; j < MG.numNodes; j++){
-            if(MG.arcs[i][j] == 65535){
-                cout << "     M ";
-            }else{
-                cout << setw(6) << MG.arcs[i][j] << " ";
-            }
-        }
-        cout << endl << endl;
+int root(int a){            //查找某个节点的根节点
+    if(a != p[a]){
+        p[a] = root(p[a]);
+    }
+    return p[a];
+}
+
+int connected(int a, int b){            //通过判断根节点是否相同，判断是否连接
+    return root(a) == root(b);
+}
+
+void Union(int a, int b){           //连接
+    if(!connected(a, b)){
+        p[root(a)] = root(b);
     }
 }
 
-void dijkstra(MGraph& MG){
-    char ch;
-    cout << "请输入起始点：";
-    cin >> ch;
-    int start = Locate(MG, ch);
-    for(int i = 0; i < MG.numNodes; i++){           //初始化dist数组，记录当前最短路径
-        string a = "", b = "";
-        a.insert(0, 1, MG.vexs[start]);
-        b.insert(0, 1, MG.vexs[i]);
-        MG.dist[i].visited = false;
-        MG.dist[i].value = MG.arcs[start][i];               //所有value都是从选定结点到当前结点的权重
-        MG.dist[i].path = "v" + a + "-->v" + b;
-    }
-    MG.dist[start].visited = true;          //源点进入已选择集合
-    MG.dist[start].value = 0;
-    int cnt = 1;
-    while(cnt < MG.numNodes){               //选择最短的一条路径的终点进入集合
-        int now = 0;
-        int mmin = INFINITY;
-        for(int i = 0; i < MG.numNodes; i++){
-            if(!MG.dist[i].visited && MG.dist[i].value < mmin){
-                now = i;
-                mmin = MG.dist[i].value;
-            }
-        }
-        MG.dist[now].visited = true;                //进入集合
-        cnt++;
-        for(int i = 0; i < MG.numNodes; i++){           //维护dist数组，如果经过最短路径集合的路径小于直接路径则更新。
-            if(!MG.dist[i].visited && MG.arcs[now][i] != 0 && MG.dist[now].value + MG.arcs[now][i] < MG.dist[i].value){
-                MG.dist[i].value = MG.dist[now].value + MG.arcs[now][i];
-                string a;
-                a.insert(0, 1, MG.vexs[i]);
-                MG.dist[i].path = MG.dist[now].path + "-->v" + a;
-            }
-        }
-    }
-    cout << "以v" << MG.vexs[start] << "为起点的最短路径为：" << endl;
-    for(int i = 0; i < MG.numNodes; i++){
-        if(MG.dist[i].value == INFINITY){
-            cout << "v" << MG.vexs[start] << "到v" << MG.vexs[i] << "没有最短路径" << endl;
-        }else{
-            cout << MG.dist[i].path << " = " << MG.dist[i].value << endl;
-        }
-    }
-}
-
-// 测试用例
-// 10 7
-// a b c d e f g
-// a g 9
-// b a 20
+// 11 7
+// a b c e d f g
+// a b 12
+// a f 16
+// a g 14
 // b c 10
-// b d 30
+// b f 7
+// c d 3
+// c f 6
 // c e 5
-// e g 15
-// e d 12
-// f e 8
-// f g 10
-// g c 18
-// b
+// d e 4
+// f e 2
+// f g 8
